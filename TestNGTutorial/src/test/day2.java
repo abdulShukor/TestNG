8c15ad81ca1db6433fa8ee8497d9e4f6d7d54a5a
abdulShukor <ashukoor12@gmail.com>
2019-08-22 20:54:55 -0400

    modified docs file, create property class

--- TestNGTutorial/src/test/day2.java
+++ TestNGTutorial/src/test/day2.java
@@ -1,15 +1,20 @@
 package test;
 
+import java.io.IOException;
+
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class day2 {
+public class day2 extends day5 {
 
 	
+	
+	
 	@Test(groups={"Smoke"})
-	public void ploan()
+	public void ploan() throws IOException
 	{
 		System.out.println("good");
+		// Login(); part of globalization 
 	}
 	
 	@BeforeTest
